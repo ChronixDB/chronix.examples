917fbd4f18dc1ff527d433fa486dced33283d414
Florian Lautenschlager <florian.lautenschlager@qaware.de>
2016-07-12 16:27:00 +0200

    Automated the release of the importer.

--- chronix-importer/importer/import.sh
+++ chronix-importer/importer/import.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Dlog4j.configurationFile=log4j2.xml -jar lib/chronix-importer-0.3.jar config.yml data/
+java -Dlog4j.configurationFile=log4j2.xml -jar lib/chronix-importer-0.4.jar config.yml data/
